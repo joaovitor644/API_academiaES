674e395c079c94653a8ca1e75da20499f2355ba8
João Vitor Santos <jv644@protonmail.com>
2025-03-25 08:06:48 -0300

    adição da autenticação com comunicação ao banco de dados

--- routes/Auth.py
+++ routes/Auth.py
@@ -1,35 +1,56 @@
-from flask import Blueprint, request, jsonify,session
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, request, jsonify, session
+from werkzeug.security import check_password_hash , generate_password_hash
+import hashlib
 from sqlalchemy.orm import sessionmaker
 from BD.bd import engine
-from sqlalchemy.sql import text
+from models.login import Login
 
 auth = Blueprint('auth', __name__)
 
-users = {
-    "casa": generate_password_hash("123", method='pbkdf2:sha256'),
-}
+# Configuração da sessão (adicionar no seu app.py ou onde você inicializa o Flask)
+# app.secret_key = 'sua_chave_secreta_aqui'
 
 @auth.route('/auth', methods=['POST'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    hashed_password = users.get(username)
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
-    if hashed_password and check_password_hash(hashed_password, password):
+    if not username or not password:
+        return jsonify({'message': 'Usuário e senha são obrigatórios'}), 400
+
+    # Busca o usuário no banco de dados
+    login1 = Login(username, password)
+    user_info = login1.Auth()
+    print(user_info)
+    if not user_info:
+        return jsonify({'message': 'Usuário não encontrado'}), 404
+
+    # Verifica a senha
+    stored_password_hash = user_info[3]  # Supondo que o hash da senha esteja na posição 3
+    if hashlib.sha256(password.encode()).hexdigest() == stored_password_hash:
         session['user'] = username
+        session['is_admin'] = user_info[4]  # Supondo que o campo is_admin esteja na posição 4
         return jsonify({'message': 'Login bem-sucedido'}), 200
-    
-    return jsonify({'message': 'Credenciais inválidas'}), 401
+
+# Suponha que user_info e password sejam definidos anteriormente no código
+    return jsonify({
+        'message': 'Credenciais inválidas'
+    }), 401
+
 
 @auth.route('/logout', methods=['POST'])
 def logout():
     session.pop('user', None)
+    session.pop('is_admin', None)
     return jsonify({'message': 'Logoff bem-sucedido'}), 200
 
 @auth.route('/session', methods=['GET'])
 def get_session():
     user = session.get('user')
-    if user:
-        return jsonify({'permission':'OK', 'user':user, 'isAdm':True}), 200
-    return jsonify({'permission': 'ERR'}), 401 
+    is_admin = session.get('is_admin')
+    login1 = Login(user, '_')
+    user_info = login1.Auth()
+    if user_info:
+        return jsonify({'permission': 'OK', 'user': user, 'isAdm': is_admin}), 200
+    return jsonify({'permission': 'ERR'}), 401
